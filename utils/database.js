143e42f7f3ec8c0d8397c9e1cadb34ff877da5c4
iannacus <ian.rosas@academlo.com>
2023-07-03 21:34:56 -0600

    ssl required

--- utils/database.js
+++ utils/database.js
@@ -10,6 +10,7 @@ const db = new Sequelize({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   dialect: "postgres",
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
 });
 
 // export default db;
